60a46dc2037d50db2b78018b5ec124e144dff713
Beau Elstgeest <beau.elstgeest@gmail.com>
2019-10-18 16:01:26 +0200

    add nl translation for label

---
+++ language/nl/label.php
@@ -0,0 +1,112 @@
+<?php
+
+return array(
+
+	//GENERIC
+	"yes" => "Ja",
+	"no" => "Nee",
+
+	//DASHBOARD
+	"image_size" => "Afbeelding grootte",
+	"file_size" => "Document grootte",
+	"thumb_size" => "Thumb grootte",
+	"cache_size" => "Cache grootte",
+	"total_size" => "Totale grootte",
+
+	"theme_name" => "Thema naam",
+	"image_compression" => "Afbeelding druk",
+	"image_max_size" => "Max grootte upload",
+	"cache_engine" => "Cache motor",
+	"clear_engine" => "ClearCode motor",
+
+	"cache_all" => "Alle cache",
+	"cache_blogs" => "Blog",
+	"cache_files" => "Bestanden",
+	"cache_images" => "Afbeelding",
+	"cache_menus" => "Menu",
+	"cache_pages" => "Pagina's",
+	"cache_sizes" => "Grootten",
+	"cache_translations" => "Vertalingen",
+
+	//FILES
+	"thumbnail" => "Thumbnail",
+	"filename" => "Bestands naam",
+	"filesize" => "grootte",
+	"availability" => "Afbeelding beschikbaarheid",
+	"width" => "Breedte",
+	"height" => "Hoogte",
+	"size" => "Grootte",
+	"created_at_o" => "Aangemaakt op",
+
+	//PAGES
+
+	"pagename" => "Pagina naam",
+	"layout" => "Layout instellingen",
+	"layout_not_set" => "Layout niet ingesteld",
+	"author" => "Autheur",
+	"created_at" => "Gemaakt op",
+	"updated_at" => "Laatste wijziging",
+	"language" => "Taal",
+	"actions" => "Acties",
+	"url" => "URL: ",
+	"page_not_valid" => "Page niet ingeschakeld",
+	"page_relations" => "Verbonden pagina's",
+	"page_elements" => "Pagina elementen",
+	"page_no_elements" => "Geen elementen",
+	"page_no_relations" => "Geen betrekking",
+	"page_no_link" => "Geen link",
+
+	"pages" => "Pagina's",
+	"blogs" => "Blog posts",
+	"products" => "Producten",
+
+	//ROLES
+
+	"rolename" => "Rol",
+	"level" => "Niveau",
+
+	//USERS
+
+	"username" => "Gebruikers naam",
+	"email" => "Email",
+
+	//MENU
+
+	"menuname" => "Menu naam",
+	"menunested" => "Hiërarchisch",
+
+	//BANNER
+	
+	"bannername" => "Banner naam",
+
+	//DOWNLOAD
+	
+	"downloadname" => "Download naam",
+
+	//GALLERY
+	
+	"galleryname" => "Gallerij naam",
+	"thumblabel" => "Thumbnail type",
+
+	//BLOG
+
+	"blogname" => "Post naam",
+	"datetime_on" => "Start datum",
+	"datetime_off" => "Eind datum",
+	"page_owners" => "Gepubliceerd op pagina's",
+	"blog_links" => "Linked to post",
+
+	//TAGS
+
+	"tagname" => "Tag naam",
+
+	//EDITOR
+	
+	"editor" => "Redacteur",
+
+	//TABLES
+
+	"pages" => "Pagina's",
+	"blogs" => "Blog",
+
+);
